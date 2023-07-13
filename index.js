80acea20c4b0de21e34df8c5bd724a6f01ad4b16
kineticbeast69 <139427930+kineticbeast69@users.noreply.github.com>
2023-07-13 10:52:23 +0530

    added index.html,index.css,index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".text",{
+    strings:["GROWTH","STRATEGY","CAREER","STARTUP","TECHNOLOGY","SUPPORT","BUSINESS"],
+    typedspeed:150,
+    backspeed:150,
+    loop:true
+})
+var typed = new Typed(".community-change",{
+    strings:["Community","Guild","Network","Circle","Group"],
+    typedspeed:100,
+    backspeed:100,
+    loop:true
+})
